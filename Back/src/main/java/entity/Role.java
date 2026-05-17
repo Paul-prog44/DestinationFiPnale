52464570b01bd94bd2396170199efa1e9cff19ef
Kenza HATEM <kenza.hatem@startum.academy>
2026-05-17 18:03:12 +0200

    authent

---
+++ Back/src/main/java/entity/Role.java
@@ -0,0 +1,25 @@
+package entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String name; // ROLE_USER, ROLE_ADMIN
+
+}
