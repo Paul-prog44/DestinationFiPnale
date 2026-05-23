ba901b2017040212dd1ab81f0db147d2eebb5ba8
Gerald <saintouenfr@hotmail.com>
2026-05-23 12:19:41 +0200

    Add create and addFlightToBooking to bopoking service

--- Back/src/main/java/com/example/Back/controller/BookingController.java
+++ Back/src/main/java/com/example/Back/controller/BookingController.java
@@ -3,6 +3,8 @@ package com.example.Back.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.example.Back.dto.BookingCreationRequest;
+import com.example.Back.dto.BookingDto;
 import com.example.Back.dto.BookingSearchByUserIdRequest;
 import com.example.Back.dto.BookingSearchByUserIdResponse;
 import com.example.Back.service.BookingService;
@@ -19,5 +21,10 @@ public class BookingController {
     @PostMapping("/search")
     public ResponseEntity<BookingSearchByUserIdResponse> searchByUserId(@RequestBody BookingSearchByUserIdRequest request) {
         return ResponseEntity.ok(bookingService.findByUserId(request));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<BookingDto> create(@RequestBody BookingCreationRequest request) {
+        return ResponseEntity.ok(bookingService.create(request));
     }
 }
--- Back/src/main/java/com/example/Back/service/BookingService.java
+++ Back/src/main/java/com/example/Back/service/BookingService.java
@@ -1,14 +1,23 @@
 package com.example.Back.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.Back.dto.BookingCreationRequest;
 import com.example.Back.dto.BookingDto;
 import com.example.Back.dto.BookingSearchByUserIdRequest;
 import com.example.Back.dto.BookingSearchByUserIdResponse;
 import com.example.Back.model.Booking;
+import com.example.Back.model.CarBooking;
+import com.example.Back.model.FlightBooking;
+import com.example.Back.model.RoomBooking;
+import com.example.Back.model.User;
 import com.example.Back.repository.BookingRepository;
+import com.example.Back.repository.FlightBookingRepository;
+import com.example.Back.repository.UserRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +27,10 @@ import lombok.RequiredArgsConstructor;
 public class BookingService {
     
     private final BookingRepository bookingRepository;
+    private final UserRepository userRepository;
+    private final FlightBookingRepository flightBookingRepository;
+    //TODO private final RoomBookingRepository roomBookingRepitory:
+    //TODO private final CarBookingRepository carBookingRepository;
 
     public BookingSearchByUserIdResponse findByUserId(BookingSearchByUserIdRequest request) {
         Integer userId = request.getUserId();
@@ -44,5 +57,57 @@ public class BookingService {
 
         return new BookingSearchByUserIdResponse(bookingDtos);
 
+    }
+
+    public BookingDto create(BookingCreationRequest request) {
+
+        Booking booking = new Booking();
+
+        User user = userRepository.getReferenceById(request.getUserId());
+        FlightBooking flightBooking = null;
+        RoomBooking roomBooking = null;
+        CarBooking carBooking = null;
+
+        booking.setUser(user);
+        booking.setFlightBooking(flightBooking);
+        booking.setCarBooking(carBooking);
+        booking.setRoomBooking(roomBooking);
+        booking.setCreatedAt(LocalDateTime.now());
+        booking.setStatus("PENDING");
+
+        Booking savedBooking = bookingRepository.save(booking);
+
+        return BookingDto.builder()
+        .id(savedBooking.getId())
+        .userId(savedBooking.getUser().getId())
+        .flightBookingId(null)
+        .roomBookingId(null)
+        .carBookingId(null)
+        .createdAt(savedBooking.getCreatedAt())
+        .status(savedBooking.getStatus())
+        .build();
+    }
+
+    @Transactional //Atomicté de transaction
+    public BookingDto addFlightToBooking(Integer bookingId, Integer flightBookingId) {
+        Booking booking  = bookingRepository.findById(bookingId)
+        .orElseThrow(() -> new EntityNotFoundException("Cette réservation n'existe pas"));
+
+        FlightBooking flightBooking = flightBookingRepository.findById(flightBookingId)
+        .orElseThrow(() -> new EntityNotFoundException("Cette réservation de vol n'existe pas"));
+
+        booking.setFlightBooking(flightBooking);
+
+        Booking savedBooking = bookingRepository.save(booking);
+
+        return BookingDto.builder()
+        .id(savedBooking.getId())
+        .userId(savedBooking.getUser().getId())
+        .flightBookingId(flightBooking.getId())
+        .roomBookingId(null)
+        .carBookingId(null)
+        .createdAt(savedBooking.getCreatedAt())
+        .status(savedBooking.getStatus())
+        .build();
     }
 } 
